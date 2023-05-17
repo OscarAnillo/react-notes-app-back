516d1c0d70d6c5c703fb9f1c3ed709dc0f95a9f3
Oscar Anillo <oscarani14@gmail.com>
2023-05-17 11:23:18 -0500

    add a try catch block to delete, cors

--- Routes/notes.js
+++ Routes/notes.js
@@ -18,15 +18,15 @@ router.post("/",  async (req, res) => {
     }
 })
 
-//Delete a note
+// Delete a note
 router.delete("/:id", async (req, res) => {
-    const deleteNote =  await Note.findById(req.params.id);
-    if(deleteNote) {
-        await Note.deleteOne()
-        res.status(200).json("The note was deleted")
-    } else {
-        res.status(403).json("There was an error!")
+    try {
+        const deleteNote =  await Note.findById(req.params.id);
+        deleteNote.deleteOne();
+        res.status(200).json("The note was deleted");
+    } catch(err) {
+        res.status(500).json(err)
     }
-})
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors")
 dotenv.config();
 
 const noteRoute = require("./Routes/notes")
@@ -8,6 +9,7 @@ const noteRoute = require("./Routes/notes")
 
 const PORT = 3005;
 
+app.use(cors());
 app.use(express.json())
 app.use("/api/notes", noteRoute)
 
